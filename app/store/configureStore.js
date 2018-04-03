b0270b01fdfc92cd1bc3e5537a70229f1475a921
sameetmo <mohanty0@purdue.edu>
2018-04-03 10:51:54 -0700

    missing import for redux compose

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import logger from './logger'
 import rootReducer from '../reducers'
