809357bdcf1680f64928b029bbfa7b55b3d632e2
mohanty0 <mohanty0@purdue.edu>
2018-04-03 01:06:41 -0700

    adding oauth-config.js without client secret

---
+++ config/oauth-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'client_id' : '97a1a289c5e14fc991938d6ea059ea99', 
+	'client_secret' : '',
+    'redirect_uri' : 'http://localhost:8888/callback',
+    'stateKey' : 'spotify_auth_state',
+    'scope' : 'user-read-private user-read-email user-library-read user-read-playback-state user-modify-playback-state',
+}
+
+
